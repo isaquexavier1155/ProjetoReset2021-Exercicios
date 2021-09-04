1bcbb09b467f8a31ac5f4e797f7e615bafefd26d
isaquexavier1155 <isaque.ixs@gmail.com>
2021-09-03 23:21:44 -0300

    Mario

--- Exercício-Mário-Implementacao-GanhoEstamina-PerdaEstamina-MetodoCrescer/src/MarioBrosEstamina/MarioBros.java
+++ Exercício-Mário-Implementacao-GanhoEstamina-PerdaEstamina-MetodoCrescer/src/MarioBrosEstamina/MarioBros.java
@@ -15,7 +15,8 @@ package MarioBrosEstamina;
     }
 
 	 //Metodo Construtor que recebe valor para os atributos:
-	 public MarioBros(String nom, int ida, double est, float alt) {this.nome=nom;
+	 public MarioBros(String nom, int ida, double est, float alt) {
+		this.nome=nom;
 		this.idade=ida;
 		this.estamina= est;
 		this.altura= alt;
